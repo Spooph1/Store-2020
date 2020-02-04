0d92398c6945b97811e69b1a99d54a7c08155830
Simon Juul <simon@documentary.dk>
2020-02-04 14:32:04 +0100

    Samme som før

--- Store/Store/Game.cs
+++ Store/Store/Game.cs
@@ -8,5 +8,27 @@ namespace Store
 {
     class Game
     {
+        /*
+        public img[] ShowcaseImg;
+        public int Price;
+        public int SalesPer;
+        public object DLC;
+
+        Void Start()
+        {
+            Get the img Array and show the on the games site
+
+            if(DLC exists)
+            {
+                Get the DLC and add it to the game site
+            }
+        }
+
+        void OnClick()
+        {
+            Add to cart
+        }
+
+    */
     }
 }
--- Store/Store/Showcase.cs
+++ Store/Store/Showcase.cs
@@ -8,5 +8,24 @@ namespace Store
 {
     class Showcase
     {
+        /*
+        public object[] Game; 
+
+        Void Start()
+        {
+            //Get games from another class and add them to the array
+        }
+
+        public void CirleTroughGames()
+        {
+            //Circles trough the game array to show the games on showcase
+        }
+
+        public void OnClick()
+        {
+            //Go to the gamesite 
+        }
+
+        */
     }
 }
